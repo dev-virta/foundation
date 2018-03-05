b431a637377d1f0a40d1b866099a79524c55645c
Juha Salo <juha.salo@solinor.com>
2018-03-05 12:13:01 +0200

    Update foundation to 4.2

--- src/Illuminate/Foundation/Console/TinkerCommand.php
+++ src/Illuminate/Foundation/Console/TinkerCommand.php
@@ -1,5 +1,6 @@
 <?php namespace Illuminate\Foundation\Console;
 
+use Boris\Boris;
 use Illuminate\Console\Command;
 
 class TinkerCommand extends Command {
@@ -46,7 +47,7 @@ class TinkerCommand extends Command {
 	{
 		$this->setupBorisErrorHandling();
 
-		with(new \Boris\Boris('> '))->start();
+		(new Boris('> '))->start();
 	}
 
 	/**
@@ -91,6 +92,10 @@ class TinkerCommand extends Command {
 			// loop going so we can keep executing commands. However, when a fatal
 			// error occurs, we have no choice but to bail out of this routines.
 			catch (\Exception $e)
+			{
+				$this->error($e->getMessage());
+			}
+			catch (\Throwable $e)
 			{
 				$this->error($e->getMessage());
 			}
